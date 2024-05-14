bde01056ff9129db600cb22c828d542bf147c43f
Ahmed Bouras <ahmed.bouras@outlook.fr>
2024-05-14 18:06:38 -0400

    login page html added

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to our store!</h1>
+
+      <form>
+        <h2>Log in</h2>
+
+        <input type="text" placeholder="Enter your name" required />
+        <button type="submit">Access to your account</button>
+      </form>
+    </div>
+  )
+}
