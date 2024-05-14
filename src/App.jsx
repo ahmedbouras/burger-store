b03428be2c83ac563ccd10a7665d96f2f22f1bf9
Ahmed Bouras <ahmed.bouras@outlook.fr>
2024-05-14 18:19:37 -0400

    state and functions added to welcome user after submitting username

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
 export default function App() {
+  const [username, setUsername] = useState("");
+
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Hello ${username}`);
+    setUsername("");
+  }
+
   return (
     <div>
       <h1>Welcome to our store!</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Log in</h2>
 
-        <input type="text" placeholder="Enter your name" required />
+        <input
+        type="text"
+        placeholder="Enter your name"
+        onChange={handleChange}
+        required />
         <button type="submit">Access to your account</button>
       </form>
     </div>
