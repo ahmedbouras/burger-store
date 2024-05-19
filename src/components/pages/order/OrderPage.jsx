351dc48f1c73f48e4320808284fc8485695f4f39
Ahmed Bouras <ahmed.bouras@outlook.fr>
2024-05-19 11:54:43 -0400

    ✨ ORDER > Add navigate hook to go to Login page if user want to log out

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,11 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function OrderPage() {
   let urlParams = useParams();
+  let navigate = useNavigate();
   
+  const handleClick = () => {
+    navigate("/");
+  }
+
   return (
     <div>
       <h1>Hello {urlParams.name}</h1>
+      <button onClick={handleClick}>Log out</button>
     </div>
   );
 }
