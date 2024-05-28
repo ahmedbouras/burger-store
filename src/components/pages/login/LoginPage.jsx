244b92626875c19c1d6170c0dede353f80370a09
Ahmed Bouras <ahmed.bouras@outlook.fr>
2024-05-27 20:48:57 -0400

    💄 F03: LoginPage > Add background image

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
 import LoginForm from "./LoginForm";
+import backgroundImage from "../../../assets/F03-burger-background.jpg"
 
 export default function LoginPage() { 
-  return <LoginForm />
+  return (
+    <Container>
+      <LoginForm />
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  height: 100vh;
+  background: url(${backgroundImage}) no-repeat center / cover;
+`
