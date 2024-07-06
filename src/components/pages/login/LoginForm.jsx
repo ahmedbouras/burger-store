331d8e5f0f52f7e168d7f9c783440955dbeabd71
Ahmed Bouras <ahmed.bouras@outlook.fr>
2024-07-06 13:23:20 -0400

    💄 F03: LoginForm > Add style to Form

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function LoginForm() {
   const [username, setUsername] = useState("");
@@ -16,19 +17,66 @@ export default function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <FormStyled onSubmit={handleSubmit}>
       <h1>Welcome to our store!</h1>
-      <br />
+      <hr />
       <h2>Log in</h2>
 
-      <input
+      <div>
+      {/* <IoPersonCircleOutline /> */}
+        <input
         type="text"
         placeholder="Enter your name"
         value={username}
         onChange={handleChange}
         required
       />
+      </div>
       <button type="submit">Access to your account</button>
-    </form>
+    </FormStyled>
   );
 }
+
+const FormStyled = styled.form`
+display: flex;
+flex-direction: column;
+padding-top: 60px;
+  color: white;
+  text-align: center;
+
+  h1 {
+    font-size: 48px;
+    font-weight: 700;
+  }
+
+  hr {
+    border: 1px solid #F56A2C;
+    width: 400px;
+    margin-top: 32px;
+    margin-bottom: 40px;
+  }
+
+  h2 {
+    font-size: 36px;
+    font-weight: 700;
+  }
+
+  input {
+    width: 400px;
+    height: 55px;
+    margin: 18px 0;
+    border-radius: 5px;
+    border: 1px solid white;
+    outline: none;
+  }
+
+  button {
+    cursor: pointer;
+    width: 400px;
+    height: 55px;
+    background-color: orange;
+    color: white;
+    border-radius: 5px;
+    border: 1px solid orange;
+  }
+`
--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,18 +1,22 @@
 import styled from "styled-components";
 import LoginForm from "./LoginForm";
-import backgroundImage from "../../../assets/F03-burger-background.jpg"
+import backgroundImage from "../../../assets/F03-burger-background.jpg";
 import Logo from "../../Logo";
 
-export default function LoginPage() { 
+export default function LoginPage() {
   return (
     <Container>
       <Logo />
       <LoginForm />
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   height: 100vh;
   background: url(${backgroundImage}) no-repeat center / cover;
-`
+`;
