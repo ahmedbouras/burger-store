e35388ff41c61303fe486748229c8e7cb7d8b40e
Ahmed Bouras <ahmed.bouras@outlook.fr>
2024-07-16 21:35:03 -0400

    💄 F04: ORDER > OrderPage > Add initial style

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,13 +1,20 @@
 import { useParams } from "react-router-dom";
 import ButtonReturn from "../../ButtonReturn";
+import styled from "styled-components";
+import theme from "../../../theme"
 
 export default function OrderPage() {
   const { username } = useParams();
 
   return (
-    <div>
+    <OrderPageStyled>
       <h1>Hello {username}</h1>
       <ButtonReturn text={"Log out"} />
-    </div>
+    </OrderPageStyled>
   );
 }
+
+const OrderPageStyled = styled.div`
+  background-color: ${theme.colors.primary};
+  height: 100vh;
+`
