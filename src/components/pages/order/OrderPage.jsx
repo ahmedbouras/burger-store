d703e09ca626bab06dc8d8fc3e207b9524ec3bc6
Ahmed Bouras <ahmed.bouras@outlook.fr>
2024-07-16 21:49:46 -0400

    ✨ F04: ORDER > Create layout

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,15 +1,21 @@
 import { useParams } from "react-router-dom";
 import ButtonReturn from "../../ButtonReturn";
 import styled from "styled-components";
-import theme from "../../../theme"
+import theme from "../../../theme";
+import Navbar from "../../reusable-ui/Navbar";
+import Main from "./Main";
 
 export default function OrderPage() {
   const { username } = useParams();
 
   return (
     <OrderPageStyled>
-      <h1>Hello {username}</h1>
-      <ButtonReturn text={"Log out"} />
+      <div className="container">
+        <Navbar />
+        <Main />
+        {/* <h1>Hello {username}</h1>
+        <ButtonReturn text={"Log out"} /> */}
+      </div>
     </OrderPageStyled>
   );
 }
@@ -17,4 +23,8 @@ export default function OrderPage() {
 const OrderPageStyled = styled.div`
   background-color: ${theme.colors.primary};
   height: 100vh;
-`
+
+  .container {
+    width: 1500px;
+  }
+`;
