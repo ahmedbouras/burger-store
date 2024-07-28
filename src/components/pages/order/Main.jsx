1fc9324cead98de44b1bb03defe9fd0ae0898427
Ahmed Bouras <ahmed.bouras@outlook.fr>
2024-07-28 15:35:09 -0400

    ✨ F05: Create Card component and configure layout in Main component

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,16 +1,42 @@
 import styled from "styled-components";
 import theme from "../../../theme";
+import Card from "../../reusable-ui/Card";
 
 export default function Main() {
   return (
-    <MainStyled></MainStyled>
-  )
+    <MainStyled>
+      <div className="card-container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 50px;
+  padding-bottom: 50px;
   flex: 1;
   background-color: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
+  overflow-y: scroll;
+
+  .card-container {
+    display: grid;
+    grid-template-columns: repeat(4, 240px);
+    gap: 60px 85px;
+  }
 `;
---
+++ src/components/reusable-ui/Card.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default function Card() {
+  return <CardStyled>Card</CardStyled>;
+}
+
+const CardStyled = styled.div`
+  height: 330px;
+  width: 240px;
+  background-color: red;
+`;
