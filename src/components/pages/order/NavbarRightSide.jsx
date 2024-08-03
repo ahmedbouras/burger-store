881b09fae88c7a23831aa11bfb249542ff0a8016
Ahmed Bouras <ahmed.bouras@outlook.fr>
2024-08-03 11:14:08 -0400

    ♻️ F06: ORDER > Navbar > Create Profile component to add user info into it

--- src/components/pages/order/NavbarRightSide.jsx
+++ src/components/pages/order/NavbarRightSide.jsx
@@ -1,20 +1,13 @@
 import styled from "styled-components";
 import theme from "../../../theme";
-import { IoPersonCircleOutline } from "react-icons/io5";
-import { Link } from "react-router-dom";
 import ToggleButton from "../../reusable-ui/ToggleButton";
+import Profile from "./Profile";
 
 export default function NavbarRightSide({ username }) {
   return (
     <NavbarRightSideStyled>
       <ToggleButton />
-      <div className="profile">
-        <p>
-          Hey, <span>{username}</span>
-        </p>
-        <Link to="/">Log out</Link>
-      </div>
-      <IoPersonCircleOutline className="icon" />
+      <Profile username={username} />
     </NavbarRightSideStyled>
   );
 }
@@ -22,37 +15,4 @@ export default function NavbarRightSide({ username }) {
 const NavbarRightSideStyled = styled.div`
   display: flex;
   align-items: center;
-
-  .profile {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    color: ${theme.colors.greyBlue};
-
-    span {
-      color: ${theme.colors.primary};
-      font-weight: ${theme.fonts.weights.bold};
-    }
-
-    a {
-      font-size: ${theme.fonts.size.XXS};
-      text-decoration: none;
-
-      &:hover {
-        text-decoration: underline;
-        text-underline-offset: 4px;
-      }
-
-      &:visited {
-        color: ${theme.colors.greyBlue};
-      }
-    }
-  }
-
-  .icon {
-    height: 36px;
-    width: 36px;
-    color: ${theme.colors.greyBlue};
-    margin-left: 10px;
-  }
 `;
---
+++ src/components/pages/order/Profile.jsx
@@ -0,0 +1,57 @@
+import { IoPersonCircleOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import theme from "../../../theme";
+
+export default function ({ username }) {
+  return (
+    <ProfileStyled>
+      <div className="profile-info">
+        <p>
+          Hey, <span>{username}</span>
+        </p>
+        <Link to="/">Log out</Link>
+      </div>
+      <IoPersonCircleOutline className="icon" />
+    </ProfileStyled>
+  );
+}
+
+const ProfileStyled = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 50px;
+
+  .profile-info {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    color: ${theme.colors.greyBlue};
+
+    span {
+      color: ${theme.colors.primary};
+      font-weight: ${theme.fonts.weights.bold};
+    }
+
+    a {
+      font-size: ${theme.fonts.size.XXS};
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: underline;
+        text-underline-offset: 4px;
+      }
+
+      &:visited {
+        color: ${theme.colors.greyBlue};
+      }
+    }
+  }
+
+  .icon {
+    height: 36px;
+    width: 36px;
+    color: ${theme.colors.greyBlue};
+    margin-left: 10px;
+  }
+`;
