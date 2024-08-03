5200e6c0cce0c57c42fd0b2139b0c6b7fd2b156f
Ahmed Bouras <ahmed.bouras@outlook.fr>
2024-08-03 18:22:30 -0400

    🎨 F06: ORDER > NAVBAR > Create Toast component to separate code

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,41 +1,40 @@
 import styled from "styled-components";
 import ToggleButton from "../../../reusable-ui/ToggleButton";
 import Profile from "./Profile";
-import { useEffect, useState } from "react";
-import { toast, ToastContainer } from "react-toastify";
+import { useState } from "react";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import theme from "../../../../theme";
+import ToastAdmin from "./ToastAdmin";
 
 export default function NavbarRightSide({ username }) {
-  const [toggle, setToggle] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
 
-  const notify = () => {
-    toast.info("Admin mode enabled", {
-      theme: "dark",
-      position: "bottom-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
+  const notifyAdminMode = () => {
+    if (!isAdmin) {
+      toast.info("Admin mode enabled", {
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+    }
+    setIsAdmin(!isAdmin)
   };
-
-  useEffect(() => {
-    if (toggle) notify();
-  }, [toggle]);
 
   return (
     <NavbarRightSideStyled>
       <ToggleButton
-        isChecked={toggle}
-        onToggle={() => setToggle(!toggle)}
+        isChecked={isAdmin}
+        onToggle={notifyAdminMode}
         labelIfChecked="Disable Admin mode"
         labelIfUnchecked="Enable Admin mode"
       />
       <Profile username={username} />
-      <ToastContainer className="toaster" bodyClassName="body-toast" />
+      <ToastAdmin />
     </NavbarRightSideStyled>
   );
 }
@@ -43,22 +42,4 @@ export default function NavbarRightSide({ username }) {
 const NavbarRightSideStyled = styled.div`
   display: flex;
   align-items: center;
-
-  .toaster {
-    max-width: 300px;
-  }
-
-  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
-    background: ${theme.colors.background_dark};
-  }
-
-  .body-toast {
-    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
-      margin-right: 20px;
-      margin-left: 5px;
-    }
-    div {
-      line-height: 1.3em;
-    }
-  }
 `;
