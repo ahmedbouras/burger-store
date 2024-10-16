b637c9c2c8f92413d039ca832c0df25472f54d4f
Ahmed Bouras <ahmed.bouras@outlook.fr>
2024-10-16 07:06:26 -0400

    🚚 F07: Move products into Menu components

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -1,27 +1,14 @@
 import styled from "styled-components";
 import theme from "../../../../theme";
-import Card from "../../../reusable-ui/Card";
-import { fakeMenu } from "../../../../fakeData/fakeMenu";
-import { useState } from "react";
 import Admin from "./Admin/Admin";
+import Menu from "./Menu";
 
 export default function Main() {
-  const [products, setProducts] = useState(fakeMenu);
-
   return (
     <MainStyled>
       {/* <div className="basket">Basket</div> */}
       <div className="menu-and-admin">
-        <div className="card-container">
-          {products.map((product) => (
-            <Card
-              key={product.id}
-              image={product.imageSource}
-              title={product.title}
-              price={product.price}
-            />
-          ))}
-        </div>
+        <Menu />
         <Admin />
       </div>
     </MainStyled>
@@ -31,7 +18,6 @@ export default function Main() {
 const MainStyled = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  /* flex: 1; */
   height: calc(95vh - 10vh);
   background-color: ${theme.colors.background_white};
   box-shadow: ${theme.borderRadius.strong};
@@ -46,18 +32,5 @@ const MainStyled = styled.div`
     position: relative;
     display: grid;
     overflow-y: hidden;
-    .card-container {
-      display: grid;
-      justify-content: center;
-      grid-template-columns: repeat(auto-fit, 240px);
-      gap: 60px 85px; // delete 85px
-      padding: 50px;
-      overflow-y: scroll;
-      //-ms-overflow-style: none; /* for Internet Explorer, Edge */
-      scrollbar-width: none; /* for Firefox */
-      &::-webkit-scrollbar {
-        display: none; // TODO : Verify if it's necessary
-      }
-    }
   }
 `;
